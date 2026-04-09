d79474073d48c1b70cc5c77cf8f3f45ebe382f9f
Adwyte <adwyte28@gmail.com>
2026-04-09 14:51:19 +0530

    db-url

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Make sure to replace special characters with percent-encoding in passwords
 # @ is %40
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:akrSQL%4005@localhost/aws-paglu"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:Organ-db123@localhost:5432/organ_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
